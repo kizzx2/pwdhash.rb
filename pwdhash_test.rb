6effd33f53d960d2ea783f6299fcab549663cd04
Chris Yuen <chris@kizzx2.com>
2014-05-03 18:47:50 +0800

    Revamped test file to be actually functional

--- pwdhash_test.rb
+++ pwdhash_test.rb
@@ -1,18 +1,18 @@
-require 'test/unit'
+require 'minitest/unit'
+require 'minitest/pride'
+require 'minitest/autorun'
+
 require './lib/pwdhash/pwdhash'
 
-class PwdHashTest < Test::Unit::TestCase
-  
-  def test_should_hash_password_and_domain
-    expected = '0AMstxYHYz'
-    actual = get_hashed_password('password', 'realm')
-    assert_equal expected, actual
+class PwdHashTest < MiniTest::Unit::TestCase
+  def test_passwords
+    [
+      ['v0F0B', 'foo', 'skype.com'],
+      ['paZTVGZwtewiq1+uCk', 'a l0ng p4assw0rd', 'google.com'],
+      ['nRDL7WNyFODhF29gAkNmpA', 'qwertyuiop0987654321', 'google.com'],
+      ['edi6wHWRQVA1rK8o9zaluwAAAA', 'qwertyuiop0987654321bingo', 'google.com'],
+    ].each do |expected, password, realm|
+      assert_equal expected, get_hashed_password(password, realm)
+    end
   end
-
-  def test_should_apply_constraints
-    expected = '0AMstxYHYz'
-    actual = apply_constraints(hash='MstxYHYzGq0jEjzfTbaFrQ', size=10, nonalphanumeric=false)
-    assert_equal expected, actual
-  end
-
 end
